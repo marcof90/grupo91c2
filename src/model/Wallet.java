a80b4dfcbee3f6725c36a000fb23d554e0e46d2f
marcof90 <ingmarcosanabria@gmail.com>
2021-07-19 11:58:05 -0500

    Code documentation

--- src/model/Wallet.java
+++ src/model/Wallet.java
@@ -2,23 +2,49 @@ package model;
 
 public class Wallet {
     
+    /**
+     * Cantidad de dinero actual en la billetera
+     */
     private int saldo;
+
 
     public Wallet() {
         super();
         saldo = 0;
     }
 
+    /**
+     * Devuelve el saldo actual de la billetera
+     * @return saldo actual de la billetera
+     */
+    public int getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * Permite cambiar el saldo actual por un valor que llega por parametro 
+     * @param saldo representa el nuevo saldo de la billetera
+     */
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * Metodo que permite guardar una cantidad de dinero dada por parametro en la billetera
+     * @param valor cantidad de dinero que se guardara en la billetera
+     * @return "cantidad no valida" en caso de que valor <= 0.
+     *          "Exito! Ahora tienes $" + saldo, en caso de que se pueda guardar el dinero
+     */
     public String guardarDinero(int valor){
-        if(valor < 0){
+        if(valor <= 0){
             return "Cantidad no válida";
         }
-        saldo += valor;
+        saldo += valor;//saldo = saldo + valor;
         return "Exito! Ahora tienes $" + saldo;
     }
 
     public String retirarDinero(int valor){
-        if(valor < 0){
+        if(valor <= 0){
             return "Cantidad no válida";
         }
         if(valor > saldo){
